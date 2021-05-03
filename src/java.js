d5753b93ea47d9eee66b49c173398a9094a05459
ninacostandc <ninacostandc@gmail.com>
2021-05-03 08:22:07 +0100

    remove image and clean js

--- src/java.js
+++ src/java.js
@@ -2,6 +2,8 @@ function showTemp (response){
   let temperature = response.data.main.temp;
   let displayTemp = ("mainTemperature");
   displayTemp.innerHTML = temperature;
+  let iconElement = document.querySelector ("#weather-icon");
+  iconElement.setAttribute (src,`http://openweathermap.org/img/wn/10d@2x.png`);
 }
 
 function showCity (response) {
@@ -15,22 +17,6 @@ function showCity (response) {
 let form = document.querySelector("#search-form")
 form.addEventListener ("submit", showCity)
 
-function showPosition (position) {
-  let lat= position.coords.latitude;
-  let lon= position.coords.longitude;
-  let country = document.querySelector (".country");
-  country.innerHTML = `${position.country}`;
-  let apiKey= "3ce60030f6a7cefb14f74f4e525a83ac";
-  let apiUrl= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemp)
-}
-
-function getCurrentPosition(){
-  navigator.geolocation.getCurrentPosition(showPosition);  
-}
-
-let button = document.querySelector ("button");
-button.addEventListener ("click", getCurrentPosition)
 
 let now = new Date();
 let hours = now.getHours();
